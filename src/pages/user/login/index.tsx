0e52c2a88376695bfa89f23c816a2df621447424
徐涛焘 <xutaotao1108@outlook.com>
2020-07-04 11:34:29 +0800

    init project

---
+++ src/pages/user/login/index.tsx
@@ -0,0 +1,81 @@
+import { Alert } from 'antd';
+import React, { useState } from 'react';
+import { connect, Dispatch } from 'umi';
+import { StateType } from '@/models/login';
+import { LoginParamsType } from '@/services/login';
+import { ConnectState } from '@/models/connect';
+import LoginForm from './components/Login';
+
+import styles from './style.less';
+
+const { Tab, UserName, Password, Submit } = LoginForm;
+interface LoginProps {
+  dispatch: Dispatch;
+  userLogin: StateType;
+  submitting?: boolean;
+}
+
+const LoginMessage: React.FC<{
+  content: string;
+}> = ({ content }) => (
+  <Alert
+    style={{
+      marginBottom: 24,
+    }}
+    message={content}
+    type="error"
+    showIcon
+  />
+);
+
+const Login: React.FC<LoginProps> = (props) => {
+  const { userLogin = {}, submitting } = props;
+  const { status, type: loginType } = userLogin;
+  const [type, setType] = useState<string>('account');
+
+  const handleSubmit = (values: LoginParamsType) => {
+    const { dispatch } = props;
+    dispatch({
+      type: 'login/login',
+      payload: { ...values, type },
+    });
+  };
+  return (
+    <div className={styles.main}>
+      <LoginForm activeKey={type} onTabChange={setType} onSubmit={handleSubmit}>
+        <Tab key="account" tab="账户密码登录">
+          {status === 'error' && loginType === 'account' && !submitting && (
+            <LoginMessage content="账户或密码错误（admin/ant.design）" />
+          )}
+
+          <UserName
+            name="userName"
+            placeholder="用户名: admin or user"
+            rules={[
+              {
+                required: true,
+                message: '请输入用户名!',
+              },
+            ]}
+          />
+          <Password
+            name="password"
+            placeholder="密码: ant.design"
+            rules={[
+              {
+                required: true,
+                message: '请输入密码！',
+              },
+            ]}
+          />
+        </Tab>
+        <Submit loading={submitting}>登录</Submit>
+      </LoginForm>
+    </div>
+  );
+};
+
+export default connect(({ login, loading }: ConnectState) => ({
+  userLogin: login,
+  submitting: loading.effects['login/login'],
+}))(Login);
