c58397eb1a6008c117f452e09c1bd6f48f0d978e
徐涛焘 <xutaotao1108@outlook.com>
2020-08-06 21:21:22 +0800

    fix: login out error

--- src/models/login.ts
+++ src/models/login.ts
@@ -1,9 +1,9 @@
-import { stringify } from 'querystring';
 import { history, Reducer, Effect } from 'umi';
 
 import { fakeAccountLogin } from '@/services/login';
 import { setAuthority } from '@/utils/authority';
 import { getPageQuery } from '@/utils/utils';
+
 
 export interface StateType {
   status?: 'ok' | 'error';
@@ -65,12 +65,13 @@ const Model: LoginModelType = {
       const { redirect } = getPageQuery();
       // Note: There may be security issues, please note
       if (window.location.pathname !== '/user/login' && !redirect) {
-        history.replace({
-          pathname: '/user/login',
-          search: stringify({
-            redirect: window.location.href,
-          }),
-        });
+        // history.replace({
+        //   pathname: '/user/login',
+        //   search: stringify({
+        //     redirect: window.location.href,
+        //   }),
+        // });
+        history.push('/user/login')
       }
     },
   },
